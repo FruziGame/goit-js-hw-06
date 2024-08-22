7067c0a11d5ebbf9f118484cff967450e0bccf35
Hlib <grossogleb@gmail.coml>
2024-08-22 19:18:43 +0300

    done

--- js/task-2.js
+++ js/task-2.js
@@ -2,20 +2,44 @@
 console.log("task-2")
 
 
-function calcAverageCalories(days) {
+class Storage {
 
-   if (days.length === 0) {
-      return 0;
+   #items
+
+   constructor(items) {
+      this.#items = items
    }
 
-   let totalCalories = 0
-
-   for (const item of days) {
-      totalCalories += item.calories
+   getItems() {
+      return this.#items
    }
 
-   return totalCalories / days.length
-      
+   addItem(newItem) {
+      this.#items.push(newItem)
+   }
+
+   removeItem(itemToRemove) {
+      if (this.#items.includes(itemToRemove)) {
+         const index = this.#items.indexOf(itemToRemove)
+         this.#items.splice(index, 1)
+      }
+   }
+
+
 }
+
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
 
